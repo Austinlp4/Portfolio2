cce2433fe1f0e4c0d35e2f5e3e9c8c6f6632cc87
Austinlp4 <apendergrast44@gmail.com>
2018-12-21 09:30:37 -0600

    style nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
 import { Route, withRouter, NavLink } from 'react-router-dom';
-import './App.css';
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav>
-          <NavLink>
+        <Nav>
+          <NavLink to='/about'>
             About
           </NavLink>
-          <NavLink>
+          <NavLink to='/works'>
             Works
           </NavLink>
-          <NavLink>
+          <NavLink to='/skills'>
             Skills
           </NavLink>
-          <NavLink>
+          <NavLink to='/art'>
             Graphic Design/ Art
           </NavLink>
-          <NavLink>
+          <NavLink to='/contact'>
             Contact
           </NavLink>
-        </nav>
+        </Nav>
         
       </div>
     );
   }
 }
+
+const Nav = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  a{
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+  }
+`;
+
 
 export default withRouter(App);
