76b2c09172e301bba4f2f6577dfcf7056178e326
Austinlp4 <apendergrast44@gmail.com>
2018-12-21 09:26:25 -0600

    Set up Nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { Route, withRouter, NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav>
+          <NavLink>
+            About
+          </NavLink>
+          <NavLink>
+            Works
+          </NavLink>
+          <NavLink>
+            Skills
+          </NavLink>
+          <NavLink>
+            Graphic Design/ Art
+          </NavLink>
+          <NavLink>
+            Contact
+          </NavLink>
+        </nav>
         
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
