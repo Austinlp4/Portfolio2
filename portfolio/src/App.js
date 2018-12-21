28c50ce5810738c513bf72c47090e27627423137
Austinlp4 <apendergrast44@gmail.com>
2018-12-21 13:18:45 -0600

    progess

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { Route, withRouter, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import About from './components/about/About';
+
+const Home = (props) => {
+    return(
+      <Greet>
+        <h1>Austin Pendergrast</h1>
+        <h2>Full Stack Web Developer</h2>
+      </Greet>
+    )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppContainer>
         <Nav>
+        <NavLink to='/'>
+            Home
+          </NavLink>
           <NavLink to='/about'>
             About
           </NavLink>
@@ -16,29 +29,63 @@ class App extends Component {
           <NavLink to='/skills'>
             Skills
           </NavLink>
-          <NavLink to='/art'>
-            Graphic Design/ Art
-          </NavLink>
           <NavLink to='/contact'>
             Contact
           </NavLink>
         </Nav>
-        
-      </div>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+      </AppContainer>
     );
   }
 }
 
 const Nav = styled.div`
   display: flex;
-  width: 100%;
+  width: 90%;
   justify-content: space-between;
+  padding: 1% 3%;
   a{
     text-decoration: none;
-    color: black;
+    color: rgba(243, 239, 239, 1);
+    text-shadow: 1px 1.5px rgba(138, 135, 135, .8);
+    font-weight: bold;
+    font-size: 1.3rem;
+    &:hover{
+      color: white;
+      text-shadow: 1px 1.5px rgba(138, 135, 135, 1);
+    }
+  }
+`;
+
+const Greet = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-self: center;
+  align-self: center;
+  margin-top: 200px;
+  h1{
+    background-color: rgba(138, 135, 135, .4);
+    color: transparent;
+    text-shadow: 0px 2px 1.5px rgba(243, 239, 239, 1);
+    -webkit-background-clip: text;
+     -moz-background-clip: text;
+          background-clip: text;
+    font-weight: bold;
+    font-size: 5rem;
+  }
+  h2 {
+    color: rgba(243, 239, 239, 1);
+    text-shadow: 1px 1.5px rgba(138, 135, 135, .8);
     font-weight: bold;
   }
 `;
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+`;
 
 export default withRouter(App);
---
+++ portfolio/src/components/about/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+class About extends React.Component {
+    render(){
+        return (
+            <div>
+                <Banner>
+                    
+                </Banner>
+            </div>
+        );
+    }
+}
+
+const Banner = styled.div`
+  
+`;
+
+export default About;
