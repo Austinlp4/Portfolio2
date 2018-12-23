11ba38e9ebb02b9a5f4172e851513c2ea2942205
Austinlp4 <apendergrast44@gmail.com>
2018-12-22 21:33:31 -0600

    Add content to about page

--- portfolio/src/components/about/About.js
+++ portfolio/src/components/about/About.js
@@ -10,6 +10,14 @@ class About extends React.Component {
                     <div>
                         <Profilepic src={Avatar} alt="Profile picture"/>
                     </div>
+                    <h3>About Me</h3>
+                    <p>
+                        I'm currently located in Fayetteville,AR. I have a lovely wife, Sydney, and a beatiful daughter,
+                         Cyan (2yrs old). I am an art enthusiast and practice frequently. I think that my eye for detail 
+                         has made me a better developer, and developing has helped me find an entirely new medium. I constantly 
+                         try to learn new languages and frameworks, as well as try out new stacks. There most certainly is just not 
+                         enough time in a day. Currently pursuing knowledge related to UI/UX design, Vue.js, and MongoDB
+                    </p>
                 </Banner>
             </div>
         );
